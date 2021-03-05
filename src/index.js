cdfee1a40d811ba843bfb24bf586422b0c78e715
李博修 <liboxiu@dreamofalice.club>
2021-03-05 17:05:53 +0800

    done

--- src/index.js
+++ src/index.js
@@ -10,8 +10,24 @@ import {
   useParams,
   withRouter
 } from "react-router-dom";
+import axios from 'axios';
+
+// axios.defaults.baseURL = 'https://xxxxx';
+
+axios.interceptors.request.use(request => {
+  console.log(request);
+  if (request.headers) {
+    request.headers.token = localStorage.getItem('token')
+  } else {
+    request.headers = {
+      token: localStorage.getItem('token'),
+    }
+  }
+  return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
 
 
-ReactDOM.render(<Router><App /></Router>, document.querySelector('#root'));
-
-
+ReactDOM.render(<Router><App/></Router>, document.querySelector('#root'));
